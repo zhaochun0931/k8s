27118f8fa36ae05860cba276836e29cf8b7fe881
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-08 22:29:41 +0800

    Update init-k8s.sh

--- init-k8s.sh
+++ init-k8s.sh
@@ -17,6 +17,13 @@ kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/
 # root@HOSTNAME:~#
 
 
+# install local-path PVC driver
+kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+kubectl annotate storageclass local-path storageclass.kubernetes.io/is-default-class=true
+
+
+
+
 
 kubectl describe node NODENAME
 
