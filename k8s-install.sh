6738f44a4901a6dadc09aaec90b415200f39fc2e
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-09 22:09:34 +0800

    Update and rename install-k8s.sh to k8s-install.sh

--- k8s-install.sh
+++ k8s-install.sh
@@ -10,7 +10,7 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --de
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 
-sudo apt-get update
-sudo apt-get install -y kubelet kubeadm kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 
