31816706610355c399129666e6dadf1aea22175c
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-09 22:11:06 +0800

    Update k8s-install.sh

--- k8s-install.sh
+++ k8s-install.sh
@@ -7,7 +7,7 @@ apt-get install -y apt-transport-https ca-certificates curl -y
 
 curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" |  tee /etc/apt/sources.list.d/kubernetes.list
 
 
 apt-get update
