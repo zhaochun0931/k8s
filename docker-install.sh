7edcd6d667e8616c4bc9490020468d8435542be2
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-19 14:11:42 +0800

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -21,3 +21,37 @@ docker run hello-world
 
 
 
+
+
+
+
+
+
+
+# ubuntu 22.04
+
+# cat /etc/lsb-release
+DISTRIB_ID=Ubuntu
+DISTRIB_RELEASE=22.04
+DISTRIB_CODENAME=jammy
+DISTRIB_DESCRIPTION="Ubuntu 22.04.2 LTS"
+#
+#
+#
+#
+# cat /etc/os-release
+PRETTY_NAME="Ubuntu 22.04.2 LTS"
+NAME="Ubuntu"
+VERSION_ID="22.04"
+VERSION="22.04.2 LTS (Jammy Jellyfish)"
+VERSION_CODENAME=jammy
+ID=ubuntu
+ID_LIKE=debian
+HOME_URL="https://www.ubuntu.com/"
+SUPPORT_URL="https://help.ubuntu.com/"
+BUG_REPORT_URL="https://bugs.launchpad.net/ubuntu/"
+PRIVACY_POLICY_URL="https://www.ubuntu.com/legal/terms-and-policies/privacy-policy"
+UBUNTU_CODENAME=jammy
+#
+
+
