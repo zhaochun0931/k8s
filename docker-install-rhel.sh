a71aea4948009f0fcc59fdc21eb9f873b4288218
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-19 22:42:08 +0800

    Update docker-install-rhel.sh

--- docker-install-rhel.sh
+++ docker-install-rhel.sh
@@ -6,6 +6,8 @@ yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
-
+systemctl enable docker
 systemctl start docker
+
+
 docker run hello-world
