49f19eb8d34c7398dfc008458f88e7c1163f81d2
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-20 22:46:34 +0800

    Update and rename init-k8s.sh to crio-flannel/init-k8s.sh

--- crio-flannel/init-k8s.sh
+++ crio-flannel/init-k8s.sh
@@ -6,17 +6,9 @@ kubeadm init --pod-network-cidr=10.244.0.0/16
 # only install network plugin in the master node
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
-# root@HOSTNAME:~# kubectl get pods -A
-# NAMESPACE      NAME                                READY   STATUS    RESTARTS   AGE
-# kube-flannel   kube-flannel-ds-xmtlx               1/1     Running   0          27s
-# kube-system    coredns-5d78c9869d-72jc9            1/1     Running   0          82s
-# kube-system    coredns-5d78c9869d-95644            1/1     Running   0          82s
-# kube-system    etcd-rabbitmq1                      1/1     Running   1          95s
-# kube-system    kube-apiserver-rabbitmq1            1/1     Running   1          95s
-# kube-system    kube-controller-manager-rabbitmq1   1/1     Running   0          95s
-# kube-system    kube-proxy-7nkhz                    1/1     Running   0          82s
-# kube-system    kube-scheduler-rabbitmq1            1/1     Running   1          95s
-# root@HOSTNAME:~#
+kubectl get pods -A
+
+
 
 
 # install local-path PVC driver
