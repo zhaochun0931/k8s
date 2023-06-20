9a46c921d688324f70fb74e2fa82d072c23b25bc
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-06-20 14:57:03 +0800

    Update init-k8s.sh

--- init-k8s.sh
+++ init-k8s.sh
@@ -37,5 +37,24 @@ kubectl taint nodes NODENAME node-role.kubernetes.io/control-plane:NoSchedule-
 
 
 kubectl cluster-info
+
 kubeadm token create --print-join-command
+
+
+
+# configuration
+/etc/kubernetes/manifests/etcd.yaml
+/etc/kubernetes/manifests/kube-apiserver.yaml
+/etc/kubernetes/manifests/kube-controller-manager.yaml
+/etc/kubernetes/manifests/kube-scheduler.yaml
+
+root@hostname:~# ls -al /etc/kubernetes/manifests
+total 24
+drwxr-xr-x 2 root root 4096 Jun 20 06:44 .
+drwxr-xr-x 4 root root 4096 Jun 20 06:44 ..
+-rw------- 1 root root 2373 Jun 20 06:44 etcd.yaml
+-rw------- 1 root root 3862 Jun 20 06:44 kube-apiserver.yaml
+-rw------- 1 root root 3279 Jun 20 06:44 kube-controller-manager.yaml
+-rw------- 1 root root 1463 Jun 20 06:44 kube-scheduler.yaml
+root@hostname:~#
 
