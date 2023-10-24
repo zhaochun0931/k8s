fa870205b4ab6f5d9eacd6851cc173669082493a
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-10-24 08:14:39 +0800

    Update and rename init-k8s.sh to 03-init-k8s.sh

--- k8s-install/03-init-k8s.sh
+++ k8s-install/03-init-k8s.sh
@@ -4,8 +4,10 @@ kubeadm init --pod-network-cidr=10.244.0.0/16
 # only install network plugin in the master node
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
-# it will generate below file
-/etc/cni/net.d/10-flannel.conflist
+
+
+# it will generate below file 
+# /etc/cni/net.d/10-flannel.conflist
 
 
 kubectl get pods -A
