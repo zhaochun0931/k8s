7178e072aa3f0030ccf6d197e8eb21cf1ebbc7a2
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-10-26 08:31:05 +0800

    Create quick-install.sh

---
+++ k8s-install/quick-install.sh
@@ -0,0 +1,9 @@
+wget https://raw.githubusercontent.com/zhaochun-vmware/k8s/main/k8s-install/01-crio-install.sh
+
+wget https://raw.githubusercontent.com/zhaochun-vmware/k8s/main/k8s-install/02-k8s-install-ubuntu.sh
+
+bash 01-crio-install.sh
+
+bash 02-k8s-install-ubuntu.sh
+
+kubeadm init --pod-network-cidr=10.244.0.0/16
