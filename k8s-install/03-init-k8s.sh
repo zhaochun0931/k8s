1c6bafe44ac609633bebb898feb9f0346cadd019
zhaochun-vmware <126732652+zhaochun-vmware@users.noreply.github.com>
2023-11-21 11:48:31 +0800

    Update 03-init-k8s.sh

--- k8s-install/03-init-k8s.sh
+++ k8s-install/03-init-k8s.sh
@@ -1,6 +1,12 @@
 kubeadm init --pod-network-cidr=10.244.0.0/16
 
+  mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
+
+
+  
 # only install network plugin in the master node
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
