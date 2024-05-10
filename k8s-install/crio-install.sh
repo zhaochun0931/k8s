27ed0d19fa15001132fa627901b6da763c6fec41
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-05-10 12:31:35 +0800

    Create crio-install.sh

---
+++ k8s-install/crio-install.sh
@@ -0,0 +1,24 @@
+# Install dependencies for adding the repositories
+apt-get update
+apt-get install -y software-properties-common curl
+
+
+# Add the Kubernetes repository
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key |
+    gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" |
+    tee /etc/apt/sources.list.d/kubernetes.list
+
+
+# Add the CRI-O repository
+curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key |
+    gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
+
+echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" |
+    tee /etc/apt/sources.list.d/cri-o.list
+
+
+# Install the packages
+apt-get update
+apt-get install -y cri-o kubelet kubeadm kubectl
