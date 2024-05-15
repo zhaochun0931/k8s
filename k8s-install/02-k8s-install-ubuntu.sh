e05f0d4539dea85a27e14520000a2b720428cadb
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-05-15 11:41:12 +0800

    Update 02-k8s-install-ubuntu.sh

--- k8s-install/02-k8s-install-ubuntu.sh
+++ k8s-install/02-k8s-install-ubuntu.sh
@@ -13,15 +13,15 @@
 
 # install Kubernetes v1.27
 
-
+export k8sversion=v1.27
 
 sudo apt-get update
 
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.27/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+curl -fsSL https://pkgs.k8s.io/core:/stable:/$(k8sversion)/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/$(k8sversion)/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
