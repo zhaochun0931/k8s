eee30abdfd2c04e5e62a0a5dc8914adf4f0fff69
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-05-15 11:37:20 +0800

    Update crio-install.sh

--- k8s-install/crio-install.sh
+++ k8s-install/crio-install.sh
@@ -3,14 +3,6 @@ apt-get update
 apt-get install -y software-properties-common curl
 
 
-# Add the Kubernetes repository
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key |
-    gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" |
-    tee /etc/apt/sources.list.d/kubernetes.list
-
-
 # Add the CRI-O repository
 curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
