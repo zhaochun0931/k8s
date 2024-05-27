725f50677f41e26348b8088bd9a0834dfe35e377
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-05-27 14:28:34 +0800

    Update crio-install.sh

--- k8s-install/crio-install.sh
+++ k8s-install/crio-install.sh
@@ -42,6 +42,6 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 
 
 apt-get update
-apt-get install -y cri-o kubelet kubeadm kubectl
+apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
     
