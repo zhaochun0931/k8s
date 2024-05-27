f8502cee357d2314de6d414923809ff4c1257afc
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-05-27 14:14:08 +0800

    Update crio-install.sh

--- k8s-install/crio-install.sh
+++ k8s-install/crio-install.sh
@@ -4,11 +4,15 @@ apt-get install -y software-properties-common curl
 
 
 # Add the CRI-O repository
+
+
 curl -fsSL https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/Release.key |
     gpg --dearmor -o /etc/apt/keyrings/cri-o-apt-keyring.gpg
-
 echo "deb [signed-by=/etc/apt/keyrings/cri-o-apt-keyring.gpg] https://pkgs.k8s.io/addons:/cri-o:/prerelease:/main/deb/ /" |
     tee /etc/apt/sources.list.d/cri-o.list
+
+
+
 
 
 # Install the packages
