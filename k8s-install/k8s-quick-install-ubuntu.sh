56db6c694607c0e7d4bdf6916d26a13031920916
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2024-08-09 08:41:31 +0800

    Update k8s-quick-install-ubuntu.sh

--- k8s-install/k8s-quick-install-ubuntu.sh
+++ k8s-install/k8s-quick-install-ubuntu.sh
@@ -4,30 +4,21 @@ echo "This script will install k8s on ubuntu 22.04"
 echo "download the install script"
 wget https://raw.githubusercontent.com/zhaochun0931/k8s/main/k8s-install/00-crio-install.sh
 wget https://raw.githubusercontent.com/zhaochun0931/k8s/main/k8s-install/02-k8s-install-ubuntu.sh
+wget https://raw.githubusercontent.com/zhaochun0931/k8s/main/k8s-install/03-init-k8s.sh
+
 
 echo "begin to install the k8s"
 bash 00-crio-install.sh
 bash 02-k8s-install-ubuntu.sh
+bash 03-init-k8s.sh
 
-kubeadm init --pod-network-cidr=10.244.0.0/16
 
-
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 
 
 # kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
 
-for i in $(echo $HOSTNAME)
-do
-kubectl taint nodes $i node-role.kubernetes.io/control-plane:NoSchedule-
-done
 
 
-kubectl get nodes
-
-
-echo "K8s installation successfully"
+echo "K8s installation is successfully and ready"
