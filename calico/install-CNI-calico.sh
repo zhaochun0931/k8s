3fb39153dc6f65c7fa22583f58411a6243b3b68d
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-22 21:08:22 +0800

    Update install-CNI-calico.sh

--- calico/install-CNI-calico.sh
+++ calico/install-CNI-calico.sh
@@ -3,3 +3,11 @@ curl https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/ca
 kubectl apply -f calico.yaml
 
 
+kubectl get ippools
+
+
+
+
+ls -al /etc/cni/net.d/calico-kubeconfig
+ls -al /etc/cni/net.d/10-calico.conflist
+
