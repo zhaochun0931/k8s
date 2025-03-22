f37b174c43bd87e1036bce1f33426a3e96834533
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-22 22:44:10 +0800

    Update install-CNI-flannel.sh

--- flannel/install-CNI-flannel.sh
+++ flannel/install-CNI-flannel.sh
@@ -7,3 +7,8 @@ echo "FLANNEL_NETWORK=10.244.0.0/16
 FLANNEL_SUBNET=10.244.0.1/24
 FLANNEL_MTU=1450
 FLANNEL_IPMASQ=true" | sudo tee /run/flannel/subnet.env
+
+
+
+ls /etc/cni/net.d/10-flannel.conflist
+ls /etc/cni/net.d/kube-flannel.yml
