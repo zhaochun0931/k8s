87795af85127a982363f189fdd92bd2a4be0267c
zhaochun0931 <77560597+zhaochun0931@users.noreply.github.com>
2025-03-22 21:32:46 +0800

    Update install-CNI-flannel.sh

--- flannel/install-CNI-flannel.sh
+++ flannel/install-CNI-flannel.sh
@@ -1,1 +1,9 @@
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
+
+
+
+echo "FLANNEL_NETWORK=10.244.0.0/16
+FLANNEL_SUBNET=10.244.1.1/24
+FLANNEL_MTU=1450
+FLANNEL_IPMASQ=true" | sudo tee /run/flannel/subnet.env
